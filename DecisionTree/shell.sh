ca87aae191c17e636cc0c8aed70801bcf6f08655
johannesRidael <35404526+johannesRidael@users.noreply.github.com>
2021-09-26 17:54:50 -0600

    Create shell.sh

---
+++ DecisionTree/shell.sh
@@ -0,0 +1,6 @@
+python3 CatTree.py 0 CatTrain.csv CatTest.csv EE
+python3 CatTree.py 0 CatTrain.csv CatTest.csv ME
+python3 CatTree.py 0 CatTrain.csv CatTest.csv GI
+python3 NumTree.py 0 NumTrain.csv NumTest.csv EE
+python3 NumTree.py 0 NumTrain.csv NumTest.csv ME
+python3 NumTree.py 0 NumTrain.csv NumTest.csv GI
